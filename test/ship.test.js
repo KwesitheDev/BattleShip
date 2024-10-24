225e18928f28b234316b4543e17e88057d1212c5
kwesitheDev <174883738+KwesitheDev@users.noreply.github.com>
2024-10-24 05:34:40 +0100

    added ship.test.js

---
+++ test/ship.test.js
@@ -0,0 +1,23 @@
+const Ship = require('../src/modules/ship');
+const jestGlobals = require('@jest/globals');
+const { describe, it, expect, beforeEach } = jestGlobals;
+
+describe('Ship', () => {
+  let ship;
+
+  beforeEach(() => {
+    ship = new Ship(3);
+  });
+
+  it('should hit a ship', () => {
+    ship.hit();
+    expect(ship.hits).toBe(1);
+  });
+
+  it('should sink a ship', () => {
+    ship.hit();
+    ship.hit();
+    ship.hit();
+    expect(ship.isSunk()).toBe(true);
+  });
+});
